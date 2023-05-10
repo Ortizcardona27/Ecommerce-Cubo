b613453578b4b9287c6069578e589c9589c29c30
Joham Hernandez <hernandezj@visualsystems.com>
2023-05-10 12:32:26 -0500

    First commit

--- js/products.js
+++ js/products.js
@@ -1,176 +1,164 @@
 const stockProductos = [
-    {
-      id: 1,
-      nombre: "Paquete 1",
-      cantidad: 1,
-      desc: "Juego plataformero, niveles dificiles",
-      precio: 1200,
-      img: "img/Crash.jpg",
-    },
-    {
-      id: 2,
-      nombre: "Paquete 2",
-      cantidad: 1,
-      desc: "Luchas con los mejores graficos",
-      precio: 1500,
-      img: "img/mortal.jpg",
-    },
-    {
-      id: 3,
-      nombre: "Paquete 3",
-      cantidad: 1,
-      desc: "Juego plataformero, niveles basicos",
-      precio: 1570,
-      img: "img/pacman.jpg",
-    },
-    {
-      id: 4,
-      nombre: "Paquete 4",
-      cantidad: 1,
-      desc: "Vive la experiencia dragon ball",
-      precio: 1000,
-      img: "img/dragonball.jpg",
-    },
-    {
-      id: 5,
-      nombre: "Naruto Ninja Storm 4",
-      cantidad: 1,
-      desc: "La historia de Naruto",
-      precio: 1200,
-      img: "img/naruto.jpg",
-    },
-    {
-      id: 6,
-      nombre: "Shingeki Final Attack",
-      cantidad: 1,
-      desc: "Eren Jeager vuelve en formato gamer...",
-      precio: 1200,
-      img: "img/shingeki.jpg",
-    },
-    {
-      id: 7,
-      nombre: "League of Legends",
-      cantidad: 1,
-      desc: "No compres esto por tu bien",
-      precio: 1400,
-      img: "img/league.jpg",
-    },
-    {
-      id: 8,
-      nombre: "Call Of Duty Warzone",
-      cantidad: 1,
-      desc: "Dispara como nunca",
-      precio: 1200,
-      img: "img/callduty.jpg",
-    },
-    {
-      id: 9,
-      nombre: "Fifa 2019",
-      cantidad: 1,
-      desc: "Juego de futbol",
-      precio: 1400,
-      img: "img/fifa.jpg",
-    },
-    {
-      id: 10,
-      nombre: "Fornite",
-      cantidad: 1,
-      desc: "Battle Royale",
-      precio: 1200,
-      img: "img/fornite.jpg",
-    },
-  ];
-  let carrito = [];
-  
-  const contenedor = document.querySelector("#contenedor");
-  const carritoContenedor = document.querySelector("#carritoContenedor");
-  const vaciarCarrito = document.querySelector("#vaciarCarrito");
-  const precioTotal = document.querySelector("#precioTotal");
-  const activarFuncion = document.querySelector("#activarFuncion");
-  const procesarCompra = document.querySelector("#procesarCompra");
-  const totalProceso = document.querySelector("#totalProceso");
-  const formulario = document.querySelector('#procesar-pago')
-  
-  if (activarFuncion) {
-    activarFuncion.addEventListener("click", procesarPedido);
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-  
+  {
+    id: 1,
+    nombre: "30GB",
+    cantidad: 1,
+    desc: "Minutos ilimitados",
+    precio: 35000,
+    img: "img/telefono.png",
+  },
+  {
+    id: 2,
+    nombre: "15GB",
+    cantidad: 1,
+    desc: "Minutos ilimitados",
+    precio: 24900,
+    img: "img/telefono.png",
+  },
+  {
+    id: 3,
+    nombre: "10GB",
+    cantidad: 1,
+    desc: "Minutos ilimitados",
+    precio: 19900,
+    img: "img/telefono.png",
+  },
+  {
+    id: 4,
+    nombre: "3.5GB",
+    cantidad: 1,
+    desc: "Minutos ilimitados",
+    precio: 9900,
+    img: "img/telefono.png",
+  },
+  {
+    id: 5,
+    nombre: "4GB de navegación",
+    cantidad: 1,
+    desc: "Telefonía, Internet y televisión",
+    precio: 79900,
+    img: "img/planes.png",
+  },
+  {
+    id: 6,
+    nombre: "10GB de navegación",
+    cantidad: 1,
+    desc: "Telefonía, Internet y televisión",
+    precio: 99900,
+    img: "img/planes.png",
+  },
+  {
+    id: 7,
+    nombre: "20GB de navegación",
+    cantidad: 1,
+    desc: "Telefonía, Internet y televisión. Incluye repetidores",
+    precio: 140900,
+    img: "img/planes.png",
+  },
+  {
+    id: 8,
+    nombre: "30GB de navegación",
+    cantidad: 1,
+    desc: "Telefonía, Internet de fibra óptica y televisión satelital. Incluye repetidores",
+    precio: 200900,
+    img: "img/planes.png",
+  },
+];
+let carrito = [];
+
+const contenedor = document.querySelector("#contenedor");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+const activarFuncion = document.querySelector("#activarFuncion");
+const procesarCompra = document.querySelector("#procesarCompra");
+const totalProceso = document.querySelector("#totalProceso");
+const formulario = document.querySelector('#procesar-pago')
+
+if (activarFuncion) {
+  activarFuncion.addEventListener("click", procesarPedido);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  mostrarCarrito();
+  document.querySelector("#activarFuncion").click(procesarPedido);
+});
+if (formulario) {
+  formulario.addEventListener('submit', enviarCompra)
+}
+
+
+if (vaciarCarrito) {
+  vaciarCarrito.addEventListener("click", () => {
+    carrito.length = [];
     mostrarCarrito();
-    document.querySelector("#activarFuncion").click(procesarPedido);
   });
-  if(formulario){
-    formulario.addEventListener('submit', enviarCompra)
-  }
-  
-  
-  if (vaciarCarrito) {
-    vaciarCarrito.addEventListener("click", () => {
-      carrito.length = [];
-      mostrarCarrito();
-    });
-  }
-  
-  if (procesarCompra) {
-    procesarCompra.addEventListener("click", () => {
-      if (carrito.length === 0) {
-        Swal.fire({
-          title: "¡Tu carrito está vacio!",
-          text: "Compra algo para continuar con la compra",
-          icon: "error",
-          confirmButtonText: "Aceptar",
-        });
-      } else {
-        location.href = "compra.html";
-      }
-    });
-  }
-  
-  stockProductos.forEach((prod) => {
-    const { id, nombre, precio, desc, img, cantidad } = prod;
-    if (contenedor) {
-      contenedor.innerHTML += `
-      <div class="card mt-3" style="width: 18rem;">
-      <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+}
+
+if (procesarCompra) {
+  procesarCompra.addEventListener("click", () => {
+    if (carrito.length === 0) {
+      Swal.fire({
+        title: "¡Tu carrito está vacio!",
+        text: "Compra algo para continuar con la compra",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+      });
+    } else {
+      location.href = "compra.html";
+    }
+  });
+}
+
+stockProductos.forEach((prod) => {
+  const { id, nombre, precio, desc, img, cantidad } = prod;
+  if (contenedor) {
+    contenedor.innerHTML += `
+    <div class="card mt-3" style="width: 18rem; text-align: center">
       <div class="card-body">
         <h5 class="card-title">${nombre}</h5>
-        <p class="card-text">Precio: ${precio}</p>
-        <p class="card-text">Descripcion: ${desc}</p>
-        <p class="card-text">Cantidad: ${cantidad}</p>
+        <br>
+        <img class="card-img-top mt-2" src="${img}" alt="Card image cap">
+        <br><br>
+        <p class="card-text">${desc}</p>        
+        <p class="card-text">$ ${precio}</p>
+        
         <button class="btn btn-primary" onclick="agregarProducto(${id})">Comprar Producto</button>
       </div>
     </div>
       `;
-    }
-  });
-  
-  const agregarProducto = (id) => {
-    const existe = carrito.some(prod => prod.id === id)
-  
-    if(existe){
-      const prod = carrito.map(prod => {
-        if(prod.id === id){
-          prod.cantidad++
-        }
-      })
-    } else {
-      const item = stockProductos.find((prod) => prod.id === id)
-      carrito.push(item)
-    }
-    mostrarCarrito()
-  
-  };
-  
-  const mostrarCarrito = () => {
-    const modalBody = document.querySelector(".modal .modal-body");
-    if (modalBody) {
-      modalBody.innerHTML = "";
-      carrito.forEach((prod) => {
-        const { id, nombre, precio, desc, img, cantidad } = prod;
-        console.log(modalBody);
-        modalBody.innerHTML += `
+  }
+});
+
+//<p class="card-text">${cantidad}</p>
+
+const agregarProducto = (id) => {
+  const existe = carrito.some(prod => prod.id === id)
+
+  if (existe) {
+    const prod = carrito.map(prod => {
+      if (prod.id === id) {
+        prod.cantidad++
+      }
+    })
+  } else {
+    const item = stockProductos.find((prod) => prod.id === id)
+    carrito.push(item)
+  }
+  mostrarCarrito()
+
+};
+
+const mostrarCarrito = () => {
+  const modalBody = document.querySelector(".modal .modal-body");
+  if (modalBody) {
+    modalBody.innerHTML = "";
+    carrito.forEach((prod) => {
+      const { id, nombre, precio, desc, img, cantidad } = prod;
+      console.log(modalBody);
+      modalBody.innerHTML += `
         <div class="modal-contenedor">
           <div>
           <img class="img-fluid img-carrito" src="${img}"/>
@@ -185,45 +173,45 @@ const stockProductos = [
         
     
         `;
-      });
-    }
-  
-    if (carrito.length === 0) {
-      console.log("Nada");
-      modalBody.innerHTML = `
+    });
+  }
+
+  if (carrito.length === 0) {
+    console.log("Nada");
+    modalBody.innerHTML = `
       <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
       `;
-    } else {
-      console.log("Algo");
-    }
-    carritoContenedor.textContent = carrito.length;
-  
-    if (precioTotal) {
-      precioTotal.innerText = carrito.reduce(
-        (acc, prod) => acc + prod.cantidad * prod.precio,
-        0
-      );
-    }
-  
-    guardarStorage();
-  };
-  
-  function guardarStorage() {
-    localStorage.setItem("carrito", JSON.stringify(carrito));
+  } else {
+    console.log("Algo");
   }
-  
-  function eliminarProducto(id) {
-    const juegoId = id;
-    carrito = carrito.filter((juego) => juego.id !== juegoId);
-    mostrarCarrito();
+  carritoContenedor.textContent = carrito.length;
+
+  if (precioTotal) {
+    precioTotal.innerText = carrito.reduce(
+      (acc, prod) => acc + prod.cantidad * prod.precio,
+      0
+    );
   }
-  function procesarPedido() {
-    carrito.forEach((prod) => {
-      const listaCompra = document.querySelector("#lista-compra tbody");
-      const { id, nombre, precio, img, cantidad } = prod;
-      if (listaCompra) {
-        const row = document.createElement("tr");
-        row.innerHTML += `
+
+  guardarStorage();
+};
+
+function guardarStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function eliminarProducto(id) {
+  const juegoId = id;
+  carrito = carrito.filter((juego) => juego.id !== juegoId);
+  mostrarCarrito();
+}
+function procesarPedido() {
+  carrito.forEach((prod) => {
+    const listaCompra = document.querySelector("#lista-compra tbody");
+    const { id, nombre, precio, img, cantidad } = prod;
+    if (listaCompra) {
+      const row = document.createElement("tr");
+      row.innerHTML += `
                 <td>
                 <img class="img-fluid img-carrito" src="${img}"/>
                 </td>
@@ -232,41 +220,41 @@ const stockProductos = [
               <td>${cantidad}</td>
               <td>${precio * cantidad}</td>
               `;
-        listaCompra.appendChild(row);
-      }
-    });
-    totalProceso.innerText = carrito.reduce(
-      (acc, prod) => acc + prod.cantidad * prod.precio,
-      0
-    );
-  }
-  
-   function enviarCompra(e){
-     e.preventDefault()
-     const cliente = document.querySelector('#cliente').value
-     const email = document.querySelector('#correo').value
-  
-     if(email === '' || cliente == ''){
-       Swal.fire({
-         title: "¡Debes completar tu email y nombre!",
-         text: "Rellena el formulario",
-         icon: "error",
-         confirmButtonText: "Aceptar",
-     })
-   } else {
-  
+      listaCompra.appendChild(row);
+    }
+  });
+  totalProceso.innerText = carrito.reduce(
+    (acc, prod) => acc + prod.cantidad * prod.precio,
+    0
+  );
+}
+
+function enviarCompra(e) {
+  e.preventDefault()
+  const cliente = document.querySelector('#cliente').value
+  const email = document.querySelector('#correo').value
+
+  if (email === '' || cliente == '') {
+    Swal.fire({
+      title: "¡Debes completar tu email y nombre!",
+      text: "Rellena el formulario",
+      icon: "error",
+      confirmButtonText: "Aceptar",
+    })
+  } else {
+
     const btn = document.getElementById('button');
-  
-  // document.getElementById('procesar-pago')
-  //  .addEventListener('submit', function(event) {
-  //    event.preventDefault();
-  
-     btn.value = 'Enviando...';
-  
-     const serviceID = 'default_service';
-     const templateID = 'template_qxwi0jn';
-  
-     emailjs.sendForm(serviceID, templateID, this)
+
+    // document.getElementById('procesar-pago')
+    //  .addEventListener('submit', function(event) {
+    //    event.preventDefault();
+
+    btn.value = 'Enviando...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_qxwi0jn';
+
+    emailjs.sendForm(serviceID, templateID, this)
       .then(() => {
         btn.value = 'Finalizar compra';
         alert('Correo enviado!');
@@ -274,28 +262,28 @@ const stockProductos = [
         btn.value = 'Finalizar compra';
         alert(JSON.stringify(err));
       });
-      
-     const spinner = document.querySelector('#spinner')
-     spinner.classList.add('d-flex')
-     spinner.classList.remove('d-none')
-  
-     setTimeout(() => {
-       spinner.classList.remove('d-flex')
-       spinner.classList.add('d-none')
-       formulario.reset()
-  
-       const alertExito = document.createElement('p')
-       alertExito.classList.add('alert', 'alerta', 'd-block', 'text-center', 'col-12', 'mt-2', 'alert-success')
-       alertExito.textContent = 'Compra realizada correctamente'
-       formulario.appendChild(alertExito)
-  
-       setTimeout(() => {
-         alertExito.remove()
-       }, 3000)
-  
-  
-     }, 3000)
-   }
-   localStorage.clear()
-  
-   }
+
+    const spinner = document.querySelector('#spinner')
+    spinner.classList.add('d-flex')
+    spinner.classList.remove('d-none')
+
+    setTimeout(() => {
+      spinner.classList.remove('d-flex')
+      spinner.classList.add('d-none')
+      formulario.reset()
+
+      const alertExito = document.createElement('p')
+      alertExito.classList.add('alert', 'alerta', 'd-block', 'text-center', 'col-12', 'mt-2', 'alert-success')
+      alertExito.textContent = 'Compra realizada correctamente'
+      formulario.appendChild(alertExito)
+
+      setTimeout(() => {
+        alertExito.remove()
+      }, 3000)
+
+
+    }, 3000)
+  }
+  localStorage.clear()
+
+}
